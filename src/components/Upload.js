a15bbd13f1fb9cc918e610f51445428192bb72fc
NawImNate <jnhatch3@gmail.com>
2022-09-10 01:29:25 -0400

    added dummy data, added upload container quote

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,21 +1,122 @@
 import React from "react";
 import "../index.css";
 import Button from "@mui/material/Button";
+import "./Upload.css";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+
+function srcset(image, size, rows = 1, cols = 1) {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  };
+}
+
+const itemData = [
+  {
+    img: "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e",
+    title: "Breakfast",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1551782450-a2132b4ba21d",
+    title: "Burger",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1522770179533-24471fcdba45",
+    title: "Camera",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c",
+    title: "Coffee",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1533827432537-70133748f5c8",
+    title: "Hats",
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62",
+    title: "Honey",
+    author: "@arwinneil",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6",
+    title: "Basketball",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1518756131217-31eb79b20e8f",
+    title: "Fern",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1597645587822-e99fa5d45d25",
+    title: "Mushrooms",
+    rows: 2,
+    cols: 2,
+  },
+  {
+    img: "https://images.unsplash.com/photo-1567306301408-9b74779a11af",
+    title: "Tomato basil",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "Sea star",
+  },
+  {
+    img: "https://images.unsplash.com/photo-1471357674240-e1a485acb3e1",
+    title: "sdje",
+    cols: 2,
+  },
+];
 
 const Upload = () => {
   return (
     <div className="uploadContainer">
       <div className="left-wrapper">
         <h1>Show off fun times</h1>
-        <Button variant="outlined" component="label">
-          Upload
-          <input hidden accept="image/*" multiple type="file" />
-        </Button>
-        <Button variant="outlined">Gallery</Button>
+        <h3>“You must go on adventures to find out where you truly belong” </h3>
+        <p>– Sue Fitzmaurice</p>
+        <div className="btn-wrapper">
+          <Button className="btn" variant="" component="label">
+            Upload
+            <input hidden accept="image/*" multiple type="file" />
+          </Button>
+          <hr className="hrBreak"></hr>
+          <Button className="btn" variant="" component="label">
+            Gallery
+          </Button>
+        </div>
       </div>
       <div className="right-wrapper">
         {/* <h1>TWO</h1> */}
-        <img alt="vaca" src={require("../images/vaca_1.jpg")} />
+        {/* <img alt="vaca" src={require("../images/vaca_1.jpg")} /> */}
+        <ImageList
+          sx={{ width: 800, height: 450 }}
+          variant="quilted"
+          cols={4}
+          rowHeight={121}
+        >
+          {itemData.map((item) => (
+            <ImageListItem
+              key={item.img}
+              cols={item.cols || 1}
+              rows={item.rows || 1}
+            >
+              <img
+                className="quiltedPic"
+                {...srcset(item.img, 121, item.rows, item.cols)}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
       </div>
     </div>
   );
